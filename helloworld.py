1bfc1ec29ba9ca1c212f7fcc410747261a703646
RobertoLL07 <roberto.lobeira@enroutesystems.com>
2021-11-02 15:40:42 -0600

    variablesss

--- helloworld.py
+++ helloworld.py
@@ -1,1 +1,25 @@
-print 'hello world'
+var = "mundo"
+num = 1234
+num2 = 5678
+
+flotante = 123.4
+flotante2 = 456.31
+carac = 'c'
+carac2 = 'd'
+
+bol = True
+bol2 = False
+bol3 = None
+
+
+print(var)
+print(carac + carac2)
+print(var + carac)
+
+print(num + num2)
+print(flotante + flotante2)
+
+print(not bol)
+print(not bol2)
+print(bol or bol2)
+print(bol and bol2)
